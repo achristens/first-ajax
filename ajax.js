5f2f7fc9b99d7162064c5ae0fed675a5a4a72204
Abby Christens <achristens@gmail.com>
2017-11-14 15:36:15 -0500

    Insert response to page and print to console

--- ajax.js
+++ ajax.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
   var stepOne = document.querySelector('#step-one');
+  var pingPong = document.querySelector('#ping-pong');
+  var section  = document.querySelector('#step3456');
 
   stepOne.addEventListener('click', function(){
     $.ajax({
@@ -10,5 +12,16 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   });
 
-
+  pingPong.addEventListener('click', function(){
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/ping',
+      method: 'GET',
+      dataType: 'text'
+    }).done(function (responseData){
+      console.log(responseData);
+      var addResponse = document.createElement('p');
+      addResponse.innerHTML = responseData;
+      section.append(addResponse);
+    });
+  });
 });
